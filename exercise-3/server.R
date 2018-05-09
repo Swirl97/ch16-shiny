7b6e132d5ede18f281608e6aaab9bf6d4ac413a3
Sarah Liu <swliu79@uw.edu>
2018-05-09 16:50:34 -0700

    Went over in class, but need to review

--- exercise-3/server.R
+++ exercise-3/server.R
@@ -2,16 +2,31 @@
 
 library(shiny)
 library(ggplot2)
+
 # Create a shiny server that creates a scatterplot. 
+server <- function(input, output){
+  
+  #Making scatter plot
+  output$scatter <- renderPlot({
+    x_data <- mpg[[input$x_var]] + 
+    y_data <- mpg[[input$y_var]] +
+    p <- ggplot(mpg) +
+      geom_point(mapping = aes(x = x_data, y = y_data), 
+                 color = input$c_var,
+                 size = input$size)
+    p
+  })
+}
+
 
 # It should take as an input the x_var, y_var, color, and size
 # Save the result of `renderPlot` to output$scatter
 
     # Store x and y values to plot
-
+  
 
     # Store the title of the graph in a variable
 
     
     # Create ggplot scatter
-
+shinyServerUI(ui)
--- exercise-3/ui.R
+++ exercise-3/ui.R
@@ -1,25 +1,35 @@
 # UI for scatterplot
 library(shiny)
 
+mpg <- mtcars
+
 # Get a vector of column names (from `mpg`) to use as select inputs
 select_values <- colnames(mpg)
 
 # Create a shinyUI with a `fluidPage` layout
+ui <- fluidPage(
 
   # A page header
-
+  h1("MPG Data Visualization"),
   
   # Add a select input for the x variable
-
+  selectInput("x_var", "Choose X Variable", choices = select_values),
   
   # Add a select input for the y variable
-
+  selectInput("y_var", "Choose Y Variable", choices = select_values),
 
   # Add a sliderInput to set the size of each point
-
+  sliderInput("size", "Choose the circle size", 1, 100, 10),
 
   # Add a selectInput that allows you to select a color from a list of choices
-
+  selectInput("c_var", "Choose Color", choices = c(
+    "blue",
+    "green"
+  )),
+  plotOutput("scatter")
 
 
   # Plot the output with the name "scatter"
+  
+)
+shinyUI(ui)
